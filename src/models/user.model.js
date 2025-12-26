1381e9dd8d0ce70efcd2a85943302b3fb710dbbd
Soton Ahmed <soton371@gmail.com>
2025-12-26 13:03:07 +0600

    create user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, //cloudinary link
+        required: true,
+    },
+    coverImage: {
+        type: String, //cloudinary link
+    },
+    watchHistory: [{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }],
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    refreshToken: {
+        type: String,
+    },
+},
+    {
+        timestamps: true
+    })
+
+
+export const User = mongoose.model("User", userSchema);
+
